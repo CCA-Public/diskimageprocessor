27aa4310a6fd8efbe2878d00edb8c88057b73339
Tim Walsh <timothyryanwalsh@gmail.com>
2017-04-21 10:39:53 -0400

    Make disktype variable names consistent

--- diskimageanalyzer.py
+++ diskimageanalyzer.py
@@ -297,7 +297,7 @@ for file in sorted(os.listdir(source)):
         if raw_image == True:
             
             # run disktype on disk image, save output to disk_dir
-            disktype_txt = os.path.join(disk_dir, 'disktype.txt')
+            disktype = os.path.join(disk_dir, 'disktype.txt')
             subprocess.call("disktype '%s' > '%s'" % (diskimage, disktype_txt), shell=True)
 
             # pull filesystem info from disktype.txt
