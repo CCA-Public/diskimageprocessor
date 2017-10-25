bc2bcf3c9a01ba90e93cfb0bdcb56c90a1f99be8
Tim Walsh <timothyryanwalsh@gmail.com>
2017-10-25 18:30:53 -0400

    Add out_dir and subprocess shell=True

--- test.py
+++ test.py
@@ -43,36 +43,49 @@ class TestDiskImageProcessorIntegration(SelfCleaningTestCase):
     """
 
     def test_integration_outputs_created_tsk(self):
-        subprocess.call('python diskimageprocessor.py ./test-data/tsk/practical.floppy.dd {}'.format(self.dest_tmpdir))
+        out_dir = j(self.dest_tmpdir, 'test')
+        subprocess.call('python diskimageprocessor.py ./test-data/tsk/practical.floppy.dd {}'.format(out_dir), 
+            shell=True)
 
         # outputs
-        self.assertTrue(is_non_zero_file(j(self.dest_tmpdir, 'description.csv')))
-        self.assertTrue(is_non_zero_file(j(self.dest_tmpdir, 'diskimageprocessor-log.txt')))
+        self.assertTrue(is_non_zero_file(j(out_dir, 
+            'description.csv')))
+        self.assertTrue(is_non_zero_file(j(out_dir, 
+            'diskimageprocessor-log.txt')))
         
-        self.assertTrue(os.path.isdir(j(self.dest_tmpdir, 'SIPs')))
-        self.assertTrue(os.path.isdir(j(self.dest_tmpdir, 'SIPs', 'practical.floppy.dd')))
+        # SIP dir
+        self.assertTrue(os.path.isdir(j(out_dir, 'SIPs')))
+        self.assertTrue(os.path.isdir(j(out_dir, 'SIPs', 
+            'practical.floppy.dd')))
 
-        self.assertTrue(os.path.isdir(j(self.dest_tmpdir, 'SIPs', 'practical.floppy.dd', 
-            'objects')))
-        self.assertTrue(os.path.isdir(j(self.dest_tmpdir, 'SIPs', 'practical.floppy.dd', 
-            'objects', 'diskimage')))
-        self.assertTrue(is_non_zero_file(j(self.dest_tmpdir, 'SIPs', 'practical.floppy.dd', 
-            'objects', 'diskimage', 'practical.floppy.dd')))
-        self.assertTrue(os.path.isdir(j(self.dest_tmpdir, 'SIPs', 'practical.floppy.dd', 
-            'objects', 'files')))
-        self.assertTrue(is_non_zero_file(j(self.dest_tmpdir, 'SIPs', 'practical.floppy.dd', 
-            'objects', 'files', 'ARP.EXE')))
+        # objects
+        self.assertTrue(os.path.isdir(j(out_dir, 'SIPs', 
+            'practical.floppy.dd', 'objects')))
+        self.assertTrue(os.path.isdir(j(out_dir, 'SIPs', 
+            'practical.floppy.dd', 'objects', 'diskimage')))
+        self.assertTrue(is_non_zero_file(j(out_dir, 'SIPs', 
+            'practical.floppy.dd', 'objects', 'diskimage', 
+            'practical.floppy.dd')))
+        self.assertTrue(os.path.isdir(j(out_dir, 'SIPs', 
+            'practical.floppy.dd', 'objects', 'files')))
+        self.assertTrue(is_non_zero_file(j(out_dir, 'SIPs', 
+            'practical.floppy.dd', 'objects', 'files', 'ARP.EXE')))
 
-        self.assertTrue(os.path.isdir(j(self.dest_tmpdir, 'SIPs', 'practical.floppy.dd', 
-            'metadata')))
-        self.assertTrue(os.path.isdir(j(self.dest_tmpdir, 'SIPs', 'practical.floppy.dd', 
-            'metadata', 'submissionDocumentation')))
-        self.assertTrue(os.path.isdir(j(self.dest_tmpdir, 'SIPs', 'practical.floppy.dd', 
-            'metadata', 'submissionDocumentation', 'brunnhilde')))
-        self.assertTrue(is_non_zero_file(j(self.dest_tmpdir, 'SIPs', 'practical.floppy.dd', 
-            'metadata', 'submissionDocumentation', 'dfxml.xml')))
-        self.assertTrue(is_non_zero_file(j(self.dest_tmpdir, 'SIPs', 'practical.floppy.dd', 
-            'metadata', 'submissionDocumentation', 'disktype.txt')))
+        # metadata
+        self.assertTrue(os.path.isdir(j(out_dir, 'SIPs', 
+            'practical.floppy.dd', 'metadata')))
+        self.assertTrue(os.path.isdir(j(out_dir, 'SIPs', 
+            'practical.floppy.dd', 'metadata', 
+            'submissionDocumentation')))
+        self.assertTrue(os.path.isdir(j(out_dir, 'SIPs', 
+            'practical.floppy.dd', 'metadata', 
+            'submissionDocumentation', 'brunnhilde')))
+        self.assertTrue(is_non_zero_file(j(out_dir, 'SIPs', 
+            'practical.floppy.dd', 'metadata', 
+            'submissionDocumentation', 'dfxml.xml')))
+        self.assertTrue(is_non_zero_file(j(out_dir, 'SIPs', 
+            'practical.floppy.dd', 'metadata', 
+            'submissionDocumentation', 'disktype.txt')))
 
 
 if __name__ == '__main__':
